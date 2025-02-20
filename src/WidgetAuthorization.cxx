b5bece16ff8f33a0a4524ff3940617e9adef88ac
Alexey Chicherin <chicherin.alexey@ya.ru>
2025-02-20 15:08:18 +0300

    20.02.2025

--- src/WidgetAuthorization.cxx
+++ src/WidgetAuthorization.cxx
@@ -49,7 +49,7 @@ WidgetAuthorization::WidgetAuthorization(WidgetApplicationLogic& logic)
     IS_CONENCTED_OK
 
     QLabel* label_login_info = new QLabel("Введите имя пользователя.\nВведите пароль.");
-    pLabel->setStyleSheet("QLabel { color : red; }");
+    label_login_info->setStyleSheet("QLabel { color : red; }");
     qVBoxLayoutAuthorization->addWidget(label_login_info);
   }
 
