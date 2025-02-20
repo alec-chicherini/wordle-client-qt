8c3eb57290a665783a575c0bba8b967a2b91602c
Alexey Chicherin <chicherin.alexey@ya.ru>
2025-02-20 14:04:58 +0300

    20.02.2025

--- src/WidgetMenu.cxx
+++ src/WidgetMenu.cxx
@@ -17,7 +17,8 @@ WidgetMenu::WidgetMenu(WidgetApplicationLogic& logic)
   qHBoxLayoutAuthorizationTop->setAlignment(Qt::AlignCenter);
   QWidget* widget_aligned_center = new QWidget;
   widget_aligned_center->setMaximumWidth(330);
-  widget_aligned_center->setSizePolicy(QSizePolicy::Expanding);
+  widget_aligned_center->setSizePolicy(QSizePolicy::Expanding,
+                                       QSizePolicy::Preffered);
   qHBoxLayoutAuthorizationTop->addWidget(widget_aligned_center);
 
   QVBoxLayout* q_vbox_layout_menu = new QVBoxLayout(widget_aligned_center);
