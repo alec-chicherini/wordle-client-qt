0e010177a9c1387df2cd38b97f6daa398c2eb107
Alexey Chicherin <chicherin.alexey@ya.ru>
2025-02-20 12:55:35 +0300

    20.02.2025

--- scripts/rebuild_desktop_in_docker.sh
+++ scripts/rebuild_desktop_in_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-$(sudo apt remove wordle-client-qt -y || echo ok)
-rm result -rf
 git pull
+rm result -rf
+$(sudo apt remove wordle-client-qt -y || echo ok)
 docker build --target=qt_from_repo . -t wordle-client-qt-build-desktop
 idTempContainer=$(docker create wordle-client-qt-build-desktop)
 docker cp "$idTempContainer":/result .
